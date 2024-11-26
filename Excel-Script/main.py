f106b4bec7d1518dd7427da5b30336e545cfbaff
GWIAdmin <admin@genwealth360.com>
2024-11-26 09:47:00 -0800

    Updated main.py with initial script

--- Excel-Script/main.py
+++ Excel-Script/main.py
@@ -1,1 +1,11 @@
 # Hello World
+import pandas as pd
+from openpyxl import load_workbook
+
+# To Load in Excel Sheet
+file_path = "template.xlsx"
+workbook = load_workbook(file_path)
+sheet = workbook.active
+
+# Extraction of Data
+year = sheet["A2"].value
